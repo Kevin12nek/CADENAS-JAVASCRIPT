c51a2abccfbd8fc3fa81361f54b07a80d558c1ef
Kevin12nek <122424652+Kevin12nek@users.noreply.github.com>
2023-02-13 11:35:55 -0600

    Se agrega

---
+++ Cadenas.js
@@ -0,0 +1,27 @@
+let persona = "Kevin";
+let Apellido = "Hernandez";
+let Estudiante = `${persona} ${Apellido}    `
+
+
+console.log (persona + " " + Apellido)
+console.log(Estudiante)
+
+let estudianteMayus = Estudiante
+console.log(estudianteMayus.toUpperCase())
+console.log(estudianteMayus.toLowerCase())
+
+console.log(Estudiante.length)
+
+let nombre = "Kevin";
+console.log(nombre.charAt(0))
+
+let apellido = "Hernandez";
+console.log(apellido.charAt(8))
+
+
+console.log(Estudiante.length)
+console.log(Estudiante.trim().length)
+
+console.log(Estudiante.includes("Kevin"))
+
+
